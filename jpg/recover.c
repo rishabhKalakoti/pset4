5c3ea7a5df6973cade3ea8743e178f205e3d13c1
RISHABH KALAKOTI <2016ucp1098@mnit.ac.in>
2016-11-07 20:27:35 +0000

    just some minor changes

--- jpg/recover.c
+++ jpg/recover.c
@@ -14,23 +14,14 @@ int main(void)
 {
     // open card.raw
     FILE* infile = fopen("card.raw", "r");
+    if (infile == NULL)
+    {
+        printf("Could not open file.\n");
+        return 2;
+    }
     
     // detect the first jpeg
-    char jpeg0[4] = {0xff, 0xd8, 0xff, 0xe0};
-    char jpeg1[4] = {0xff, 0xd8, 0xff, 0xe1};
-    char firstchk[4];
-    
-    fread(firstchk, 1, 4, infile);
-    while(!(firstchk[0] == jpeg0[0] && 
-        firstchk[1] == jpeg0[1] && 
-        firstchk[2] == jpeg0[2] && 
-        (firstchk[3] == jpeg0[3] || 
-        firstchk[3] == jpeg1[3])) && firstchk[3] != EOF)
-    {
-        fseek(infile, -3, SEEK_CUR);
-        fread(firstchk, 1, 4, infile);
-    }
-    fseek(infile, -1, SEEK_CUR);
+    char jpeg0[5] = {0xff, 0xd8, 0xff, 0xe0, 0xef};
     
     // open new jpeg
     char buffer[512];
@@ -44,8 +35,9 @@ int main(void)
     {
         // write 512 bytes until a new jpeg is detected
         if (buffer[0] == jpeg0[0] && buffer[1] == jpeg0[1] && 
-        buffer[2] == jpeg0[2] && (buffer[3] == jpeg0[3] || 
-        buffer[3] == jpeg1[3]))
+            buffer[2] == jpeg0[2] && 
+            (buffer[3] >= jpeg0[3] && 
+            buffer[3] <= jpeg0[4]))
         {
             if (flag == 0)
             {
@@ -55,14 +47,13 @@ int main(void)
             {
                 fclose(outfile);
             }
-            
             char fname[8];
             sprintf(fname, "%03d.jpg", filecount);
             outfile = fopen(fname, "w");
             filecount++;
         }
         
-        fwrite(buffer, 1, 512, outfile);
+        fwrite(buffer, 512, 1, outfile);
         // find end of file
     }while(fread(buffer, 1, 512, infile) == 512);
     
